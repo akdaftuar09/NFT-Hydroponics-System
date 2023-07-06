d38b8088ab94782b485d88ee46a30cbaa44eb7d3
Akash Sinha <64327403+akdaftuar09@users.noreply.github.com>
2023-07-06 13:26:02 +0530

    Add files via upload

---
+++ TDS.ino
@@ -0,0 +1,37 @@
+
+ 
+ /**************************************************
+ 
+    Calibration CMD:
+     enter -> enter the calibration mode
+     cal:tds value -> calibrate with the known tds value(25^c). e.g.cal:707
+     exit -> save the parameters and exit the calibration mode
+ ****************************************************/
+
+#include <EEPROM.h>
+#include "GravityTDS.h"
+
+#define TdsSensorPin A1
+GravityTDS gravityTds;
+
+float temperature = 25,tdsValue = 0;
+
+void setup()
+{
+    Serial.begin(115200);
+    gravityTds.setPin(TdsSensorPin);
+    gravityTds.setAref(5.0);  //reference voltage on ADC, default 5.0V on Arduino UNO
+    gravityTds.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
+    gravityTds.begin();  //initialization
+}
+
+void loop()
+{
+    //temperature = readTemperature();  //add your temperature sensor and read it
+    gravityTds.setTemperature(temperature);  // set the temperature and execute temperature compensation
+    gravityTds.update();  //sample and calculate 
+    tdsValue = gravityTds.getTdsValue();  // then get the value
+    Serial.print(tdsValue,0);
+    Serial.println("ppm");
+    delay(1000);
+}
